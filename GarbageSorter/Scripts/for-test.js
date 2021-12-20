fd73c27d3bcf03249123edfc921c3910ee95ee82
Narine Davtyan <narine.davtyan.benivo@outlook.com>
2021-12-20 19:57:40 +0400

    Change on person registration, company registration, add jQuery codes

---
+++ GarbageSorter/Scripts/for-test.js
@@ -0,0 +1,26 @@
+﻿window.addEventListener("load", () => {
+    $("#accept").click(function () {
+        window.location.href = "company-registeration.html";
+    })
+    let radioValue = $("input[name='drone']:checked").val();
+    console.log(radioValue);
+
+    $("#huey").click(function () {
+        let radioValue = $("input[name='drone']:checked").val();
+
+        $("#my-form").attr("action", "company-registeration.html");
+        console.log($("#my-form").attr("action"));
+        console.log(radioValue);
+    })
+
+    $("#dewey").click(function () {
+        let radioValue = $("input[name='drone']:checked").val();
+
+        $("#my-form").attr("action", "person-registeration.html");
+        console.log($("#my-form").attr("action"));
+
+        console.log(radioValue);
+    })
+
+
+})
