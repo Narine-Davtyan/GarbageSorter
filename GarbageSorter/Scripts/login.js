3f0dbd43fd0d0031ec1ed486eac866a6c03a0158
Narine Davtyan <narine.davtyan.benivo@outlook.com>
2021-12-23 19:19:52 +0400

    Added "Add Image" button code, login User code, changed some files names, changed and added some styles.

---
+++ GarbageSorter/Scripts/login.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("load", function () {
+    let txt_user = $("#user");
+    let txt_pass = $("#pass");
+    let submit_btn = $("#submit_btn");
+
+    submit_btn.click(function () {
+        if (txt_user.val() && txt_pass.val()) {
+            window.location.href = "company-profile.html";
+        } else {
+            alert("User and password are empty");
+        }
+    });
+})
+
+
---
+++ GarbageSorter/Scripts/registration.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener("load", () => {
+
+    $("#company").click(function () {
+
+        $("#my-form").attr("action", "company-registration.html");
+    })
+
+    $("#person").click(function () {
+
+        $("#my-form").attr("action", "person-registration.html");
+    })
+
+
+})
